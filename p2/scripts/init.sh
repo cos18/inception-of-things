67ba315f151b8f8293d18920804feff518392c6c
Sungwoo Park <codest99@gmail.com>
2023-09-15 11:53:18 +0900

    chore: edit init.sh p2

--- p2/scripts/init.sh
+++ p2/scripts/init.sh
@@ -1,12 +1,12 @@
 curl -sfL https://get.k3s.io/ | sh -s - --cluster-init --node-ip=192.168.56.110 --write-kubeconfig-mode 644
-echo 'alias k="kubectl"' >> ~/.bashrc
-source ~/.bashrc
+echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
+echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
 
 # Install helm
-curl -o helm-v3.12.3-linux-amd64.tar.gz https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz
-chmod 777 helm-v3.12.3-linux-amd64.tar.gz
-tar -zxvf helm-v3.12.3-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
-rm -rf *
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm --kubeconfig /etc/rancher/k3s/k3s.yaml ls --all-namespaces
 
 sudo yum install -y git
+
+# k create -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
