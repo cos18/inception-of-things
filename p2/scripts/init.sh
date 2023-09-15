41e2ce22711bd17d14687d1235001ea40787d656
Sungwoo Park <codest99@gmail.com>
2023-09-15 19:36:04 +0900

    chore: adding traefik conf

--- p2/scripts/init.sh
+++ p2/scripts/init.sh
@@ -3,10 +3,12 @@ echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 
+sudo yum install -y git
+
 # Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm --kubeconfig /etc/rancher/k3s/k3s.yaml ls --all-namespaces
 
-sudo yum install -y git
+git clone https://github.com/paulbouwer/hello-kubernetes.git
 
 # k create -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
