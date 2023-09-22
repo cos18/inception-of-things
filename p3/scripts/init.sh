f0bd9ad62d704aadee69827e217c2a7bb1c4db1c
Sungwoo Park <codest99@gmail.com>
2023-09-22 18:43:43 +0900

    fix: change p3 to wait argocd deploy

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -7,8 +7,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-echo "alias sudo='sudo -E env PATH=$PATH'" >> /home/vagrant/.bashrc
-alias sudo='sudo -E env PATH=$PATH'
+echo "alias sudo='sudo -E env PATH=/usr/local/bin:$PATH'" >> /home/vagrant/.bashrc
 
 sudo /usr/local/bin/k3d cluster create p3 --api-port 6443 -p 8080:80@loadbalancer
 
@@ -21,6 +20,8 @@ sudo /usr/local/bin/kubectl create namespace dev
 sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/argocd.yaml
 sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/argocd-ingress.yaml
 
+sudo /usr/local/bin/kubectl wait --for=condition=available --timeout=600s deployments --all -n argocd
+
 sudo /usr/local/bin/kubectl -n argocd patch secret argocd-secret -p '{"stringData": {
   "admin.password": "$2a$12$ZMgNhElmwKsa9THZekEF2un3YkE73UajpDCqGrHmbguSLatxmmE7G",
   "admin.passwordMtime": "'$(date +%FT%T%Z)'"
@@ -30,6 +31,6 @@ sudo /usr/local/bin/kubectl -n argocd patch secret argocd-secret -p '{"stringDat
 sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/project.yaml
 sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/application.yaml
 
-sudo /usr/local/bin/kubectl wait --for=condition=Ready pods --all -n argocd --timeout=100s
+sudo /usr/local/bin/kubectl wait --for=condition=available --timeout=600s deployments --all -n argocd
 
 # sudo /usr/local/bin/kubectl port-forward svc/wil-playground -n dev 8888:8888
