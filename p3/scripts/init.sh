90eb659ef86dfad764367e990391119f2bcb78d8
Sungwoo Park <codest99@gmail.com>
2023-09-22 15:04:20 +0900

    chore: finish p3

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -8,14 +8,28 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 echo "alias sudo='sudo env PATH=$PATH'" >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
+alias sudo='sudo -E env PATH=$PATH'
 
-sudo k3d cluster create p3 --api-port 6443 -p 8080:80@loadbalancer
+sudo /usr/local/bin/k3d cluster create p3 --api-port 6443 -p 8080:80@loadbalancer
 
-echo "export KUBECONFIG=$(sudo k3d kubeconfig write p3)" >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
+echo "export KUBECONFIG=$(sudo /usr/local/bin/k3d kubeconfig write p3)" >> /home/vagrant/.bashrc
+export KUBECONFIG=$(sudo /usr/local/bin/k3d kubeconfig write p3)
 
-sudo kubectl create namespace argocd
+sudo /usr/local/bin/kubectl create namespace argocd
+sudo /usr/local/bin/kubectl create namespace dev
 
-sudo kubectl apply -n argocd -f /vagrant/confs/argocd.yaml
-sudo kubectl apply -n argocd -f /vagrant/confs/argocd-ingress.yaml
+sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/argocd.yaml
+sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/argocd-ingress.yaml
+
+sudo /usr/local/bin/kubectl -n argocd patch secret argocd-secret -p '{"stringData": {
+  "admin.password": "$2a$12$ZMgNhElmwKsa9THZekEF2un3YkE73UajpDCqGrHmbguSLatxmmE7G",
+  "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }
+}'
+
+sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/project.yaml
+sudo /usr/local/bin/kubectl apply -n argocd -f /vagrant/confs/application.yaml
+
+sudo /usr/local/bin/kubectl wait --for=condition=Ready pods --all -n argocd --timeout=100s
+
+# sudo /usr/local/bin/kubectl port-forward svc/wil-playground -n dev 8888:8888
